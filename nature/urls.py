ad47a9260dbc7d4d4810bdde279546939fe104ed
Terhi Salonen <terhi.salonen@haltu.fi>
2024-04-02 17:57:15 +0300

    Replace from django.conf.urls import url with from django.urls import re_path.

--- nature/urls.py
+++ nature/urls.py
@@ -1,43 +1,45 @@
-from django.conf.urls import url
+#from django.conf.urls import url
+from django.urls import re_path
+
 
 from . import views
 
 app_name = "nature"
 urlpatterns = [
-    url(
+    re_path(
         r"^feature-report/(?P<pk>\d+)/$",
         views.FeatureReportView.as_view(),
         name="feature-report",
     ),
-    url(
+    re_path(
         r"^observationseries-report/(?P<pk>\d+)/$",
         views.ObservationSeriesReportView.as_view(),
         name="observationseries-report",
     ),
-    url(
+    re_path(
         r"^feature-observations-report/(?P<pk>\d+)/$",
         views.FeatureObservationsReportView.as_view(),
         name="feature-observations-report",
     ),
-    url(
+    re_path(
         r"^feature-habitattypeobservations-report/(?P<pk>\d+)/$",
         views.FeatureHabitatTypeObservationsReportView.as_view(),
         name="feature-habitattypeobservations-report",
     ),
-    url(
+    re_path(
         r"^species-report/(?P<pk>\d+)/$",
         views.SpeciesReportView.as_view(),
         name="species-report",
     ),
-    url(
+    re_path(
         r"^species-regulations-report/(?P<pk>\d+)/$",
         views.SpeciesRegulationsReportView.as_view(),
         name="species-regulations-report",
     ),
-    url(
+    re_path(
         r"^observation-report/(?P<pk>\d+)/$",
         views.ObservationReportView.as_view(),
         name="observation-report",
     ),
-    url(r"^wfs", views.FeatureWFSView.as_view(), name="wfs"),
+    re_path(r"^wfs", views.FeatureWFSView.as_view(), name="wfs"),
 ]
